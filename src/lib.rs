f863708b0827437d8cc60aa32e04bcc1598b9dec
Andrey Dyldin <and@cesbo.com>
2019-01-13 15:20:41 +0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#[derive(Debug, Default)]
+pub struct Csa {
+    //
+}
+
+impl Csa {
+    pub fn set_cw(&mut self, cw: &[u8]) {
+        //
+    }
+
+    pub fn decrypt(&self, src: &[u8], dst: &mut [u8]) {
+        //
+    }
+}
